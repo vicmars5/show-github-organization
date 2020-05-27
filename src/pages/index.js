1c0f02221fb850f3786d0f78b2948562192af5e1
Victor Agustín Diaz Mendez <mars.vic5@gmail.com>
2020-05-27 18:31:15 -0500

    fix eslint config

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,38 +8,39 @@ const IndexPage = ({ data }) => (
   <Layout>
     <Header siteTitle={data.github.organization.name} />
 
-    <pre><code>
-      {JSON.stringify(data, null, 2)}
-         </code>
+    <pre>
+      <code>
+        {JSON.stringify(data, null, 2)}
+      </code>
     </pre>
   </Layout>
 )
 
 export default IndexPage
 export const query = graphql`
-	query {
-		github {
-			organization (login: "debtcollective") {
-				name
-				description
-				avatarUrl
-				url
-				repositories (first: 100) {
-					totalCount
-					edges {
-						node {
-							name
-							description
-							mentionableUsers (first: 1) {
-								totalCount
-							}
-							stargazers (first: 1) {
-								totalCount
-							}
-						}
-					}
-				}
-			}
-		}
-	}
+  query {
+    github {
+      organization (login: "debtcollective") {
+        name
+        description
+        avatarUrl
+        url
+        repositories (first: 100) {
+          totalCount
+          edges {
+            node {
+              name
+              description
+              mentionableUsers (first: 1) {
+                totalCount
+              }
+              stargazers (first: 1) {
+                totalCount
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 `
